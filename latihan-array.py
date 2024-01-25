4624cedc0708197aa8c9a1852d71489b85b3851a
wahyurhy <wahyurahayu82@gmail.com>
2024-01-25 07:51:35 +0700

    latihan array

---
+++ latihan-array.py
@@ -0,0 +1,11 @@
+var_arr = [1, 7, 2, 89, 3]
+left_pointer = var_arr[0]
+
+for i in range(1, len(var_arr)):
+    right_pointer = var_arr[i]
+    if right_pointer > left_pointer:
+        left_pointer = right_pointer
+
+print(left_pointer)
+
+# output: 89
