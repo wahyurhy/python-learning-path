df10904078bd1db6cd497669dc0967a60aea1d97
wahyurhy <wahyurahayu82@gmail.com>
2024-01-25 08:06:22 +0700

    kuis python array no cheating2 club

---
+++ kuis-python-array-list.py
@@ -0,0 +1,24 @@
+"""
+TODO:
+Sebuah variabel array diberikan dengan ketentuan berikut.
+- Variabel array bernama "var_array" dengan nilai dari 0 hingga 100.
+- Hitung nilai rata-rata dari elemen array tersebut.
+- Simpan hasil perhitungan dalam variabel bernama "result".
+
+Tips:
+- Rumus menghitung rata-rata adalah jumlah seluruh elemen dibagi banyaknya elemen.
+- Gunakan percabangan dan perulangan untuk mempermudah,
+  Anda tidak diperbolehkan memberikan nilai secara langsung.
+"""
+# Jangan ubah kode ini
+var_array = [i for i in range(101)]
+
+# TODO: Silakan buat kode Anda di bawah ini.
+count_all_value = 0
+for i in range(len(var_array)):
+    if i < len(var_array):
+        count_all_value += i
+result = count_all_value / len(var_array)
+print(result)
+
+# output: 50.0
