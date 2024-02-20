0552c7a3f0b55ced1e4625bf432e1397f17a8129
Wahyu Rahayu <wahyurahayu82@gmail.com>
2024-02-20 08:32:26 +0700

    latihan stock-opname

---
+++ input-python.py
@@ -0,0 +1,5 @@
+hargaMakanan = int(input("Berapa harga mie ayam per porsi? \n"))
+jumlahMakanan = int(input("Berapa porsi yang anda beli? \n"))
+totalMakanan = jumlahMakanan * hargaMakanan
+
+print(f"Harga makanan per porsi {hargaMakanan}, total harganya {totalMakanan}")
---
+++ stock-opname.py
@@ -0,0 +1,17 @@
+"""
+TODO:
+ 1. Buatlah variabel chocolateBarStock bertipe data integer yang bernilai 50.
+ 2. Tambahkan perintah untuk meminta masukan pengguna yang bertipe data integer dan simpan pada variabel chocolateBarCount.
+ 3. Gunakan ekspresi yang tepat untuk menghitung total stok camilan dan simpan pada newChocolateBarStock bertipe data integer.
+ 4. Tampilkan hasil stok baru pada console dengan teks "Stok saat ini adalah {newChocolateBarStock} buah."
+"""
+
+# Tulis kode Anda di bawah ini
+chocolateBarStock: int = 50
+chocolateBarCount: int = int(input("Masukkan nilai: "))
+newChocolateBarStock: int = chocolateBarStock - chocolateBarCount
+print(f"Stok saat ini adalah {newChocolateBarStock} buah.")
+
+# output
+# Masukkan nilai: 10
+# Stok saat ini adalah 40 buah.
